c73efc5a7397ad3de313b364ca706c7b721da9e5
jassyjr <116488124+jassyjr@users.noreply.github.com>
2023-01-23 22:30:50 +0300

    Update index.js
    ok

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 //If player 1 wins
@@ -41,7 +41,7 @@ document.getElementById("btn1").addEventListener("click",function roll() {
   
   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
   
-  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+  var randomImageSource2 = "images" + randomNumber2 + ".png";
   
   document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
   
