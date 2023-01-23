59157f6c9b1a562ea6982e781459e513d2d7c04c
jassyjr <116488124+jassyjr@users.noreply.github.com>
2023-01-23 22:49:31 +0300

    Update index.js
    ok

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImageSource2 = "images" + randomNumber2 + ".png";
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-//If player 1 wins
+document.querySelectorAll("img")[1]
+.setAttribute("src", randomImageSource2);
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩" + player1 +"Wins!";
 }
@@ -28,15 +27,15 @@ else {
 });
 document.getElementById("btn1").addEventListener("click",function roll() {
   
-  var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
   
-  var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+  var randomDiceImage = "dice" + randomNumber2 + ".png"; //dice1.png - dice6.png
   
   var randomImageSource = "images" + randomDiceImage; //images/dice1.png - images/dice6.png
   
-  var image1 = document.querySelectorAll("img")[0];
+  var image2 = document.querySelectorAll("img")[0];
   
-  image1.setAttribute("src", randomImageSource);
+  image2.setAttribute("src", randomImageSource);
   
   
   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
