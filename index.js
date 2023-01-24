6c78c32b409c43593da5560ba7656687fd7281c4
jassyjr <116488124+jassyjr@users.noreply.github.com>
2023-01-24 21:41:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ document.getElementById("prom2").innerHTML= player2;
 document.getElementById("btn1").addEventListener("click",function roll() {
   
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-var randomDiceImage1 = "images/dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
 var randomImageSource = "images" + randomDiceImage; //images/dice1.png - images/dice6.png
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1]
 .setAttribute("src", randomImageSource2);
 if (randomNumber1 > randomNumber2) {
@@ -27,7 +27,7 @@ else {
 });
 document.getElementById("btn2").addEventListener("click",function roll() {
   
-  var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
   
   var randomDiceImage = "images/dice" + randomNumber2 + ".png"; //dice1.png - dice6.png
   
