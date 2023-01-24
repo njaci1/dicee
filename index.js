bceffd87356c385a5c981fdc7d6813ac03f2b14d
jassyjr <116488124+jassyjr@users.noreply.github.com>
2023-01-24 09:54:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ document.getElementById("btn1").addEventListener("click",function roll() {
   
   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
   
-  var randomImageSource2 = "images" + randomNumber2 + ".png";
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
   
   document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
   
